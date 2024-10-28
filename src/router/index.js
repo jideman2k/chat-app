d46b11ee8ac93cb6454c28895b36b6d490a56bfb
jayblaq <16197770+jideman2k@users.noreply.github.com>
2024-10-28 20:06:06 +0100

    feature protected route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Welcome from "../views/Welcome.vue";
 import Chat from "../views/Chat.vue";
+import { auth } from "../service/firebase/config";
+
+const requireAuth = (to, from, next) => {
+  let user = auth.currentUser;
+  if (!user) {
+    next({ name: "Welcome" });
+  } else {
+    next();
+  }
+};
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +24,7 @@ const router = createRouter({
       path: "/chat",
       name: "Chat",
       component: Chat,
+      beforeEnter: requireAuth,
     },
   ],
 });
