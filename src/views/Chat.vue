1d12b218efefd46fd1466e712dbfffa445151e16
jayblaq <16197770+jideman2k@users.noreply.github.com>
2024-10-29 10:29:26 +0100

    watch the user and router to component

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -5,6 +5,9 @@
 </template>
 <script>
 import Navbar from "@/components/Navbar.vue";
+import getUser from "@/composables/getUser";
+import { useRouter } from "vue-router";
+import { watch } from "vue";
 
 export default {
   name: "Chat",
@@ -13,7 +16,15 @@ export default {
   },
 
   setup() {
-    return {};
+    const { user } = getUser();
+    const router = useRouter();
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: "Welcome" });
+      }
+    });
+
+    return { user };
   },
 };
 </script>
